03b67bfd1ecb77b919d90ee889d0daa1d1517a43
ghostmaruko <m4rc035u@gmail.com>
2022-10-31 11:52:16 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import List from "./List";
+import Details from "./Details";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />,
+  },
+
+  {
+    path: "/",
+    element: <Details />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Details = () => {
+  return (
+    <>
+      <p>Details</p>
+    </>
+  );
+};
+
+export default Details;
